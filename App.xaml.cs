bb7c497cc8a717edb51f4252624ca9904bc48335
leonplis <leonplis@educareenrollment.com>
2023-07-23 15:35:47 -0300

    Adding global app event to App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,33 @@
-﻿namespace PlagiarismDetectorMobile;
+﻿using System.Diagnostics;
+
+namespace PlagiarismDetectorMobile;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    // Define a static property that can be accessed from anywhere in your app
+    public static string AppName { get; } = "PlagiarismDetectorMobile";
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+    }
 
-		MainPage = new MainPage();
-	}
+    protected override void OnStart()
+    {
+        // Handle when your app starts
+        Debug.WriteLine("Application has started.");
+    }
+
+    protected override void OnSleep()
+    {
+        // Handle when your app sleeps
+        Debug.WriteLine("Application has gone to sleep.");
+    }
+
+    protected override void OnResume()
+    {
+        // Handle when your app resumes
+        Debug.WriteLine("Application has resumed.");
+    }
 }
 
