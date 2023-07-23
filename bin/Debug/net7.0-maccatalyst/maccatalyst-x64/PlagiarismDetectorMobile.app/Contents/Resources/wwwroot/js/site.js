3f2453dd4c707c8f9a47880049e4086a5fc3bd5d
leonplis <leonplis@educareenrollment.com>
2023-07-23 14:57:22 -0300

    First commit bit@@

---
+++ bin/Debug/net7.0-maccatalyst/maccatalyst-x64/PlagiarismDetectorMobile.app/Contents/Resources/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿
+function saveAsFile(filename, data) {
+    var blob = new Blob([data], { type: 'text/plain' });
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+}
